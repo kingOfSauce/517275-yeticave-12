840ede20ad757ae7bf973a0f9aeb8db680bd3455
kingOfSauce <dima-domashev@mail.ru>
2020-06-23 19:54:29 +0300

    Revert "Revert "Создал массив с лотами""
    This reverts commit 1a45fca8fd9e1bb44ce3a39f1f5d9124d724c69e.

--- index.php
+++ index.php
@@ -52,7 +52,47 @@ $user_name = 'Дима'; // укажите здесь ваше имя
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
+            <?php 
+                $cat = ["Ботинки", "Одежда", "Инструменты", "Разное"];
+                $lots = [
+                    [
+                        'title' => '2014 Rossignol District Snowboard',
+                        'category' => 'Доски и лыжи',
+                        'price' => '10999',
+                        'URL' => 'img/lot-1.jpg'
+                    ],
+                    [
+                        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+                        'category' => 'Доски и лыжи',
+                        'prie' => '159999',
+                        'URL' => '	img/lot-2.jpg'
+                    ],
+                    [
+                        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+                        'category' => 'Крепления',
+                        'price' => '8000',
+                        'URL' => '	img/lot-3.jpg'
+                    ],
+                    [
+                        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+                        'category' => 'Ботинки',
+                        'price' => '10999',
+                        'URL' => 'img/lot-4.jpg'
+                    ],
+                    [
+                        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+                        'category' => 'Одежда',
+                        'price' => '7500',
+                        'URL' => 'img/lot-5.jpg'
+                    ],
+                    [
+                        'title' => 'Маска Oakley Canopy',
+                        'category' => 'Разное',
+                        'price' => '5400',
+                        'URL' => 'img/lot-6.jpg'           
+                    ]
+                ];
+            ?>
             <li class="promo__item promo__item--boards">
                 <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
             </li>
