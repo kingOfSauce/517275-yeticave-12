d847c7a49a71ecdbc858943d5a1b80dfc42cc678
kingOfSauce <47989962+kingOfSauce@users.noreply.github.com>
2020-06-23 22:29:21 +0300

    Добавил функцию для форматирования цены карточки

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Дима'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -27,9 +27,22 @@ $user_name = ''; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+        <?php if($is_auth == 1): ?>
+            <div class="user-menu__logged">
+                <p><?=$user_name; ?></p>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__logout" href="#">Выход</a>
+  </div>
+        <?php else: ?>
+            <ul class="user-menu__list">
+    <li class="user-menu__item">
+      <a href="#">Регистрация</a>
+    </li>
+    <li class="user-menu__item">
+      <a href="#">Вход</a>
+    </li>
+  </ul>
+        <?php endif; ?>
         </nav>
     </div>
 </header>
@@ -39,7 +52,59 @@ $user_name = ''; // укажите здесь ваше имя
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
+            <?php 
+                $cat = ["Ботинки", "Одежда", "Инструменты", "Разное"];
+                $lots = [
+                    [
+                        'title' => '2014 Rossignol District Snowboard',
+                        'category' => 'Доски и лыжи',
+                        'price' => '10999',
+                        'URL' => 'img/lot-1.jpg'
+                    ],
+                    [
+                        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+                        'category' => 'Доски и лыжи',
+                        'price' => '159999',
+                        'URL' => 'img/lot-2.jpg'
+                    ],
+                    [
+                        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+                        'category' => 'Крепления',
+                        'price' => '8000',
+                        'URL' => '	img/lot-3.jpg'
+                    ],
+                    [
+                        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+                        'category' => 'Ботинки',
+                        'price' => '10999',
+                        'URL' => 'img/lot-4.jpg'
+                    ],
+                    [
+                        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+                        'category' => 'Одежда',
+                        'price' => '7500',
+                        'URL' => 'img/lot-5.jpg'
+                    ],
+                    [
+                        'title' => 'Маска Oakley Canopy',
+                        'category' => 'Разное',
+                        'price' => '5400',
+                        'URL' => 'img/lot-6.jpg'           
+                    ]
+                ];
+                function formatting ($num) {
+                    $num = ceil($num);
+                    if ($num < 1000) {
+                        $num = $num . "₽";
+                        print($num);
+                    } else if ($num >= 1000) {
+                        $num = number_format($num, 0, ',', ' ');
+                        $num = $num . "₽";
+                        print($num);
+                    }
+                    return 0;
+                }
+            ?>
             <li class="promo__item promo__item--boards">
                 <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
             </li>
@@ -51,17 +116,18 @@ $user_name = ''; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?php foreach ($lots as $key => $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$val['URL']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$val['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?php formatting($val['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -69,6 +135,7 @@ $user_name = ''; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -78,9 +145,11 @@ $user_name = ''; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
+            <?php foreach($lots as $key => $val): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?=$val['category']; ?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
