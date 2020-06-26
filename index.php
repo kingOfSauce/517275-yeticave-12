da0982c78351009d2da9a849cb965b827e6b89dd
kingOfSauce <47989962+kingOfSauce@users.noreply.github.com>
2020-06-26 18:31:15 +0300

    Исправил ошибки

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ $user_name = 'Дима'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <?php 
-                $cat = ["Ботинки", "Одежда", "Инструменты", "Разное"];
+                $categories = ["Ботинки", "Одежда", "Инструменты", "Разное"];
                 $lots = [
                     [
                         'title' => '2014 Rossignol District Snowboard',
@@ -104,18 +104,18 @@ $user_name = 'Дима'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach ($lots as $key => $val): ?>
+            <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$val['URL']; ?>" width="350" height="260" alt="">
+                    <img src="<?=$lot['URL']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$val['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['title']; ?></a></h3>
+                    <span class="lot__category"><?=$lot['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$val['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$lot['price']; ?>₽</span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -133,9 +133,9 @@ $user_name = 'Дима'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach($lots as $key => $val): ?>
+            <?php foreach($categories as $category): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?=$val['category']; ?></a>
+                <a href="pages/all-lots.html"><?=$category; ?></a>
             </li>
             <?php endforeach; ?>
         </ul>
