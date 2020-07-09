7d479b8217dd4e8f6aa4038143803af2efc535b4
Дима Домашев <dima-domashev@mail.ru>
2020-07-09 07:02:01 +0300

    Обновил SQL-файл

--- schema.sql
+++ schema.sql
@@ -1,35 +1,48 @@
-CREATE DATABASE YetiCave
-    DEFAULT CHARACTER SET utf8
-    DEFAULT COLLATE UTF8_GENERAL_CI;
+CREATE DATABASE IF NOT EXISTS YetiCave
+    DEFAULT CHARACTER SET utf8mb4
+    DEFAULT COLLATE utf8mb4_unicode_ci;
 
 USE YetiCave;
+-- PRAGMA FOREIGN KEY = ON;
 
-CREATE TABLE category (
-    category_name CHAR(64) UNIQUE,
-    symbol_code CHAR(64) UNIQUE
+CREATE TABLE IF NOT EXISTS category (
+    id INT PRIMARY KEY,
+    name VARCHAR(128) UNIQUE,
+    symbol_code VARCHAR(128) UNIQUE
 );
 
-CREATE TABLE lot (
+CREATE TABLE IF NOT EXISTS lot (
+    id INT PRIMARY KEY,
     date_of_create DATETIME(6),
-    title CHAR(128),
+    title VARCHAR(128),
     description TEXT,
-    img CHAR(64),
+    img VARCHAR(128),
     start_price INT,
     expiration_date DATE,
-    bet_step INT
+    bet_step INT,
+    category_id INT NOT NULL,
+    winner_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (winner_id) REFERENCES user(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-CREATE TABLE bet (
-   date DATETIME(6),
-   price INT 
+CREATE TABLE IF NOT EXISTS bet (
+    id INT PRIMARY KEY,
+    user_id INT,
+    date DATETIME(6),
+    price INT,
+    lot_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (lot_id) REFERENCES lot(id)
 ); 
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY,
     date_of_registration DATETIME(6),
-    email VARCHAR(128) UNIQUE,
-    name CHAR(64) UNIQUE,
-    password CHAR(64) UNIQUE,
+    email VARCHAR UNIQUE,
+    name VARCHAR UNIQUE,
+    password VARCHAR UNIQUE,
     contacts VARCHAR(128)
 );
-
-CREATE INDEX category_n ON category(category_name);
