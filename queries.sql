09c224ea3a7c726404db42b47932c5e4a4baa196
Дима Домашев <dima-domashev@mail.ru>
2020-07-09 23:34:52 +0300

    Добавил некоторые категории в БД

---
+++ queries.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO category SET name = "Шапки", symbol_code = "hats";
+-- INSERT INTO category SET name = "Куртки", symbol_code = "jackets";
+-- INSERT INTO category SET name = "Штаны", symbol_code = "pants";
+-- INSERT INTO category SET name = "Обувь", symbol_code = "boots";
+-- INSERT IGNORE INTO category (id, name, symbol_code) VALUES (2, "Шапки", "hats");
+-- INSERT IGNORE INTO category (name, symbol_code) VALUES ("Куртки", "jackets");
+-- INSERT IGNORE INTO category (id, name, symbol_code) VALUES (2, "Штаны", "pants");
+INSERT INTO category (name, symbol_code) VALUES ("Обувь", "boots");
