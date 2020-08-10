e3d8751af76c8bd606a80c99f1216026590f6f1b
Дима Домашев <dima-domashev@mail.ru>
2020-08-10 20:36:10 +0300

    Внес правки

--- lot.php
+++ lot.php
@@ -13,15 +13,15 @@
   $con = connection();
 
   function get_lot ($con, $id) {
-    $lots_sql = "SELECT l.id, title, img, c.name, expiration_date, description, bet_step, price FROM lot l JOIN category c ON l.category_id = c.id JOIN bet b ON l.id = b.lot_id WHERE l.id = ".$id;
-    $stmt_1 =  db_get_prepare_stmt($con, $lots_sql);
+    $lots_sql = "SELECT l.id, title, img, c.name, expiration_date, description, bet_step, price FROM lot l JOIN category c ON l.category_id = c.id JOIN bet b ON l.id = b.lot_id WHERE l.id = (?)";
+    $stmt_1 =  db_get_prepare_stmt($con, $lots_sql, $data = [$id]);
     mysqli_stmt_execute($stmt_1);
     $res = mysqli_stmt_get_result($stmt_1);
     $lots_list = mysqli_fetch_assoc($res);
     return $lots_list;
 }
 $lot = get_lot($con, $id);
-if (count($lot) == 0) {
+if (NULL === $lot) {
   http_response_code(404);
     echo "Лот не найден";
     exit();
